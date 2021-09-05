f28c0ab9f82fbd6c3dee9cc877dbf11f3be5c0ea
Elena Emelyanova <gerasimova.lennna161@gmail.com>
2021-09-05 21:51:09 +0300

    Добавлена фильтрация

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -10,12 +10,59 @@ class Home extends BaseController
 {
     public function index()
     {
-        $model = new CheckModel();
-        $data['checks'] = $model->findAll();
+        $checksModel = new CheckModel();
+        $subjectModel = new SubjectsModel();
+        $authorityModel = new SupervisoryAuthoritiesModel();
+
+        $subjectsMap = [];
+        $subjects = $subjectModel->findAll();
+        foreach ($subjects as $subject) {
+            $subjectsMap[$subject['id']] = $subject['name'];
+        }
+        $data['subjects'] = $subjectsMap;
+
+        $authorityMap = [];
+        $authority = $authorityModel->findAll();
+        foreach ($authority as $aut) {
+            $authorityMap[$aut['id']] = $aut['name'];
+        }
+        $data['authority'] = $authorityMap;
+        /* $data['supervisory'] = $authorityModel->findAll();*/
+
+        $filter = [
+            'subject_id' => $this->request->getGet('subject_id'),
+            'authority_id' => $this->request->getGet('authority_id'),
+            'duration' => $this->request->getGet('duration'),
+            'start_date' => $this->request->getGet('periodFrom'),
+            'finish_date' => $this->request->getGet('periodTo'),
+        ];
+
+        if ($filter['subject_id']) {
+            $checksModel->where('subject_id', $filter['subject_id']);
+        }
+
+        if ($filter['authority_id']) {
+            $checksModel->where('authority_id', $filter['authority_id']);
+        }
+
+        if ($filter['duration']) {
+            $checksModel->where('duration', $filter['duration']);
+        }
+
+        if ($filter['start_date']) {
+            $checksModel->where('start_date', $filter['start_date']);
+        }
+
+        if ($filter['finish_date']) {
+            $checksModel->where('finish_date', $filter['finish_date']);
+        }
+
+        $data['checks'] = $checksModel->findAll();
+        $data['filter'] = $filter;
         $data['title'] = ucfirst('Перечень плановых проверок');
 
         echo view('templates/header', $data);
-        echo view('checks_index');
+        echo view('checks_index', $data);
         echo view('templates/footer');
     }
 
@@ -23,7 +70,6 @@ class Home extends BaseController
     {
         $subjectModel = new SubjectsModel();
         $authorityModel = new SupervisoryAuthoritiesModel();
-
         $data['subjects'] = $subjectModel->findAll();
         $data['supervisory'] = $authorityModel->findAll();
 
--- app/Views/checks_index.php
+++ app/Views/checks_index.php
@@ -2,68 +2,104 @@
 <div class="container">
     <div class="content-list">
 
-        <form method="post" enctype="multipart/form-data">
+        <form action="<?= site_url('/') ?>" method="get">
             <div class="row">
                 <div class="col">
-                    <div class="form-group"><label>Наименование СМП<br>
-                            <input class="form-control" type="text" name="name"/></label><br></div>
+                    <div class="form-group"><label>Выберите СМП
+                            <select class="form-control" name="subject_id">
+                                <option value="">Все</option>
+                                <?php foreach ($subjects as $id => $name): ?>
+                                    <?php $selected = $id == $filter['subject_id']; ?>
+                                    <option value="<?= $id ?>" <?= $selected ? 'selected' : '' ?>><?= $name ?></option>
+                                <?php endforeach; ?>
+                            </select><br>
+                        </label>
+                    </div>
                 </div>
 
                 <div class="col">
-                    <div class="form-group"><label>Контролирующий орган<br>
-                            <input class="form-control" type="text" name="supervisory"/></label></div>
+                    <div class="form-group">
+                        <label>Контролирующий орган
+                            <select class="form-control" name="authority_id">
+                                <option value="">Все</option>
+                                <?php foreach ($authority as $id => $name): ?>
+                                    <?php $selected = $id == $filter['authority_id']; ?>
+                                    <option value="<?= $id ?>"<?= $selected ? 'selected' : '' ?>><?= $name ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label>Период проверки с
+                            <input class="form-control" type="date" name="periodFrom"
+                                   value="<?= $filter['start_date'] ?: '' ?>"/></label><br>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label>по
+                            <input class="form-control" type="date" name="periodTo"
+                                   value="<?= $filter['finish_date'] ?: '' ?>"/></label><br>
+                    </div>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col">
-                    <div class="form-group"><label>Период проверки с<br>
-                            <input class="form-control" type="date" name="periodFrom"/></label></div>
-                </div>
-
-                <div class="col">
-                    <div class="form-group"><label>по<br>
-                            <input class="form-control" type="date" name="periodTo"/></label><br></div>
+                    <div class="form-group"><label>Длительность проверки<br>
+                            <input class="form-control"
+                                   type="number"
+                                   name="duration"
+                                   value="<?= $filter['duration'] ?: '' ?>"
+                            /></label>
+                    </div>
                 </div>
             </div>
             <button type="submit" class="btn btn-primary">Найти</button>
+            <a class="btn btn-danger" href="<?= site_url('/') ?>">Сбросить</a>
         </form>
     </div>
 </div>
-
 
 <?= $this->section('content') ?>
 <div class="actionbutton mt-2">
     <a class="btn btn-info float-right mb20" href="/add">Создать</a>
 </div>
 
+<?php if (count($checks) == 0): ?>
+    <h2 class="text-center">По Вашему запросу ничего не найдено</h2>
+<?php else: ?>
 <table class="table table-striped">
     <thead>
     <tr>
         <th>№</th>
         <th>Проверяемый СПМ</th>
         <th>Контролирующий орган</th>
-        <th>Период проверки</th>
-        <th>Период проверки</th>
+        <th>Период проверки с</th>
+        <th>Период проверки по</th>
         <th>Длительность</th>
         <th>Действия</th>
     </tr>
     </thead>
-
     <tbody>
+
     <?php for ($i = 0; $i < count($checks); $i++): ?>
-<tr>
-    <td><?= $i+1?></td>
-    <td><?= $checks[$i]['subject_id'] ?></td>
-    <td><?= $checks[$i]['authority_id'] ?></td>
-    <td><?= $checks[$i]['start_date'] ?></td>
-    <td><?= $checks[$i]['finish_date'] ?></td>
-    <td><?= $checks[$i]['duration'] ?></td>
-    <td align="center">
-        <a class="btn btn-sm btn-info" href="<?= site_url('/edit/'.$checks[$i]['id'])?>">Редактировать</a>
-        <a class="btn btn-sm btn-danger" href="<?= site_url('home/delete/'.$checks[$i]['id'])?>">Удалить</a>
-    </td>
-</tr>
+        <tr>
+            <td><?= $i + 1 ?></td>
+            <td><?= $subjects[$checks[$i]['subject_id']] ?></td>
+            <td><?= $authority[$checks[$i]['authority_id']] ?></td>
+            <td><?= (new DateTime($checks[$i]['start_date']))->format('d-m-Y') ?></td>
+            <td><?= (new DateTime($checks[$i]['finish_date']))->format('d-m-y') ?></td>
+            <td><?= $checks[$i]['duration'] ?></td>
+            <td align="center">
+                <a class="btn btn-sm btn-info" href="<?= site_url('/edit/' . $checks[$i]['id']) ?>">Редактировать</a>
+                <a class="btn btn-sm btn-danger" href="<?= site_url('home/delete/' . $checks[$i]['id']) ?>">Удалить</a>
+            </td>
+        </tr>
     <?php endfor; ?>
+    <?php endif; ?>
     </tbody>
 </table>
